1cd313cb3fdb91b273529a9555121bb11a0ded79
Acyco <chencongyi@live.com>
2022-05-26 16:19:23 +0800

    creating-and-combining-views

---
+++ SwiftUITutorials/SwiftUITutorials/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  SwiftUITutorials
+//
+//  Created by Acyco on 2022/05/26.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+       Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.white,  lineWidth: 4)
+            )
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
